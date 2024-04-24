6819c23ce3392b7f2a57e84ede4905f1f8dc6ae9
whydenyscry <gogool124512@gmail.com>
2024-04-24 17:39:55 +0300

    add project

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
 # Stress-Tensor-Matlab
- Stress tensor, hydrostatic stress tensor, deviatoric stress tensor, invariants of stress tensor and deviatoric stress tensor, principal stresses and Mohr's diagram in Matlab
+Stress tensor, hydrostatic stress tensor, deviatoric stress tensor, invariants of stress tensor and deviatoric stress tensor, principal stresses, von Mises stress and Mohr's diagram in Matlab for the case when all components of stress tensor are given.
+The script is programmed with extended formulas for the purpose of stress-free export to other languages, if necessary.
+ 
+ ## Theoretical background
+ 
+ ### Stress Tensor
+ 
+ $$
+ \boldsymbol{\sigma} = 
+	\begin{pmatrix}
+		\sigma_x&\tau_{xy}&\tau_{xz}\\
+		\tau_{yx}&\sigma_y&\tau_{yz}\\
+		\tau_{zx}&\tau_{zy}&\sigma_z
+	\end{pmatrix}.
+ $$
+ 
+ ### Thermodynamic Pressure
+ 
+ $$
+P = -\frac{1}{3}\text{tr}\boldsymbol{\sigma}.
+ $$
+ 
+ ### Hydrostatic Stress Tensor
+ 
+ $$
+\boldsymbol{\sigma}_\text{hyd} = -P\mathbf{I}.
+ $$
+
+ ### Hydrostatic Stress Tensor
+ 
+ $$
+\boldsymbol{\sigma}' =\boldsymbol{\sigma} - \boldsymbol{\sigma}_\text{hyd}.
+ $$
+ 
+  ### Invariants of the Stress Tensor
+ 
+ $$
+	I_1 = \text{tr} \boldsymbol{\sigma},
+ $$
+ 
+  $$
+	I_2 = \frac{1}{2}\left[\text{tr}^2 \boldsymbol{\sigma} - \text{tr} \left(\boldsymbol{\sigma}^2\right)\right],
+ $$
+ 
+  $$
+	I_3 = \det \boldsymbol{\sigma}.
+ $$
+ 
+### Invariants of the Deviatoric Stress Tensor
+
+ $$
+	J_1 = \text{tr}\boldsymbol{\sigma}'=0,
+ $$
+ 
+ $$
+	J_2 = \frac{1}{2}\text{tr}\left(\boldsymbol{\sigma}'^2\right) = \frac{1}{3}I_1^2 - I_2,
+ $$
+ 
+ $$
+	J_3 = \det \boldsymbol{\sigma}' =\frac{2}{27}I_1^3 - \frac{1}{3}I_1 I_2 + I_3.
+ $$
+
+### Principal Stresses
+
+The principal stresses are eigenvalues of the stress tensor, therefore
+
+$$
+\det\left(\boldsymbol{\sigma}-\sigma\mathbf{I}\right)=0\Rightarrow\sigma^3-I_1\sigma^2+I_2\sigma-I_3=0.
+$$
+
+The solution of this equation can be given in trigonometric form:
+
+$$
+p=-\frac{1}{3}I_1^2+I_2,\quad q=-2\left(\frac{I_1}{3}\right)^3+\frac{1}{3}I_1I_2-I_3,\quad \alpha=\arccos{\left(-\frac{q}{2\sqrt{-\left(\frac{p}{3}\right)^3}}\right)},
+$$
+
+$$
+\sigma_1=\frac{1}{3}I_1 + 2\sqrt{-\frac{p}{3}}\cos{\frac{\alpha}{3}},
+$$
+
+$$
+\sigma_2= \frac{1}{3}I_1 -2\sqrt{-\frac{p}{3}}\cos{\left(\frac{\alpha}{3}+\frac{\pi}{3}\right)},
+$$
+
+$$
+\sigma_3= \frac{1}{3}I_1 -2\sqrt{-\frac{p}{3}}\cos{\left(\frac{\alpha}{3}-\frac{\pi}{3}\right)}.
+$$
+
+Further, it is assumed that the principal stresses are sorted as follows
+
+$$
+\sigma_1\geq\sigma_2\geq\sigma_3.
+$$
+
+Then invariants can be expressed in terms of principal stresses
+
+$$
+I_1=\sigma_1+\sigma_2+\sigma_3,
+$$
+
+$$
+I_2= \sigma_1\sigma_2+\sigma_2\sigma_3+\sigma_3\sigma_1,
+$$
+
+$$
+I_3= \sigma_1\sigma_2\sigma_3.
+$$
+
+### Von Mises Stress
+
+$$
+\sigma_\text{vM} = \sqrt{3J_2}= \sqrt{\frac{3}{2}\text{tr}\left(\boldsymbol{\sigma}'^2\right)} =\sqrt{\frac{\left(\sigma_1-\sigma_2\right)^2+\left(\sigma_2-\sigma_3\right)^2+\left(\sigma_3-\sigma_1\right)^2}{2}}.
+$$
+
+### Mohr's Diagram
+
+Radii of circles:
+
+$$
+R_1=\frac{1}{2}\left(\sigma_2 - \sigma_3\right),
+$$
+
+$$
+R_2= \frac{1}{2}\left(\sigma_1 - \sigma_3\right),
+$$
+
+$$
+R_3= \frac{1}{2}\left(\sigma_1 - \sigma_2\right).
+$$
+
+Centers of circles:
+
+$$
+O_1=\left(\frac{1}{2}\left(\sigma_2 + \sigma_3\right),  0\right),
+$$
+
+$$
+O_2=  \left(\frac{1}{2}\left(\sigma_1 + \sigma_3\right), 0\right),
+$$
+
+$$
+O_3= \left(\frac{1}{2}\left(\sigma_1 + \sigma_2\right), 0\right).
+$$
+
+## Example 
+
+In the script _StressTensor.m_ the plotting of the Mohr's diagram for the tensor is considered
+
+$$
+\boldsymbol{\sigma} =
+\begin{pmatrix}
+			-22.2 & 9.1 & 7.3 \\
+			9.1 & -16.9 & -4.6 \\
+			7.3 & -4.6 & 31.8 \\
+		\end{pmatrix}
+$$
+![The Mohr's Diagram](https://github.com/whydenyscry/Stress-Tensor-Matlab/blob/main/images/The_Mohrs_Diagram.png)
---
+++ StressTensor.m
@@ -0,0 +1,94 @@
+clear
+clc
+close
+
+format longG
+
+components = {-22.2 9.1 7.3 9.1 -16.9 -4.6 7.3 -4.6 31.8};
+[sigma_x, tau_xy, tau_xz, tau_yx, sigma_y, tau_yz, tau_zx, tau_zy, sigma_z] = components{:};
+
+% % symbolic
+% components = {-22.2 9.1 7.3 9.1 -16.9 -4.6 7.3 -4.6 31.8};
+% components_sym = cellfun(@sym, components, 'UniformOutput', 0);
+% [sigma_x, tau_xy, tau_xz, tau_yx, sigma_y, tau_yz, tau_zx, tau_zy, sigma_z] = components_sym{:};
+
+bf_sigma = [sigma_x tau_xy tau_xz;
+           tau_yx sigma_y tau_yz;
+           tau_zx tau_zy sigma_z]
+
+P = -(sigma_x/3 + sigma_y/3 + sigma_z/3)
+bf_sigma_hydrostatic = -P*eye(3)
+bf_sigma_prime = bf_sigma - bf_sigma_hydrostatic
+
+I_1 = sigma_x + sigma_y + sigma_z
+I_2 = sigma_x*sigma_y + sigma_x*sigma_z + sigma_y*sigma_z - tau_xy*tau_yx - tau_xz*tau_zx - tau_yz*tau_zy
+I_3 = sigma_x*sigma_y*sigma_z - sigma_z*tau_xy*tau_yx - sigma_y*tau_xz*tau_zx - sigma_x*tau_yz*tau_zy + tau_xy*tau_zx*tau_yz + tau_yx*tau_xz*tau_zy
+
+% % via bf_sigma
+% J_1 = 0
+% J_2 = sigma_x^2/3 - (sigma_x*sigma_y)/3 - (sigma_x*sigma_z)/3 + sigma_y^2/3 - (sigma_y*sigma_z)/3 + sigma_z^2/3 + tau_xy*tau_yx + tau_xz*tau_zx + tau_yz*tau_zy
+% J_3 (2*sigma_x^3)/27 - (sigma_x^2*sigma_y)/9 - (sigma_x*sigma_z^2)/9 - (sigma_x^2*sigma_z)/9 - (sigma_y*sigma_z^2)/9 - (sigma_y^2*sigma_z)/9 - (sigma_x*sigma_y^2)/9 + (2*sigma_y^3)/27 + (2*sigma_z^3)/27 + (4*sigma_x*sigma_y*sigma_z)/9 + (sigma_x*tau_xy*tau_yx)/3 + (sigma_y*tau_xy*tau_yx)/3 + (sigma_x*tau_xz*tau_zx)/3 - (2*sigma_z*tau_xy*tau_yx)/3 - (2*sigma_y*tau_xz*tau_zx)/3 - (2*sigma_x*tau_yz*tau_zy)/3 + (sigma_z*tau_xz*tau_zx)/3 + (sigma_y*tau_yz*tau_zy)/3 + (sigma_z*tau_yz*tau_zy)/3 + tau_xy*tau_zx*tau_yz + tau_yx*tau_xz*tau_zy;
+
+J_1 = 0
+J_2 = 1/3*I_1^2 - I_2
+J_3 = 2/27*I_1^3 - 1/3*I_1*I_2 + I_3
+
+p = -1/3*I_1^2 + I_2
+q = -2*(I_1/3)^3 + 1/3*I_1*I_2 - I_3
+alpha = acos(-q/(2*sqrt(-(p/3)^3)))
+
+sigma_1 = 2*sqrt(-p/3)*cos(alpha/3) + 1/3 * I_1
+sigma_2 = -2*sqrt(-p/3)*cos(alpha/3 + pi/3) + 1/3*I_1
+sigma_3 = -2*sqrt(-p/3)*cos(alpha/3 - pi/3) + 1/3*I_1
+
+sorted_pr = cellfun(@(x) sort(x, "descend"), {sigma_1, sigma_2, sigma_3}, "UniformOutput", 0)
+[sigma_1, sigma_2, sigma_3] = sorted_pr{:}
+
+% sigma_vM_via_bf_sigma = sqrt(sigma_x^2 - sigma_x*sigma_y - sigma_x*sigma_z + sigma_y^2 - sigma_y*sigma_z + sigma_z^2 + 3*tau_xy*tau_yx + 3*tau_xz*tau_zx + 3*tau_yz*tau_zy)
+% sigma_vM_via_pr = sqrt(sigma_1^2 - sigma_1*sigma_2 - sigma_1*sigma_3 + sigma_2^2 - sigma_2*sigma_3 + sigma_3^2)
+sigma_vM_via_J_2 = sqrt(3*J_2)
+
+figure()
+set(groot, "defaultAxesTickLabelInterpreter", "latex");
+set(groot, "defaultTextInterpreter", "latex");
+set(groot, "defaultLegendInterpreter", "latex");
+
+grid on;
+box on;
+hold on;
+axis equal;
+axis tight;
+xlabel('$\sigma_n$', 'FontSize', 14);
+ylabel('$\tau_n$', 'FontSize', 14);
+title("The Mohr's Diagram", 'FontSize', 16);
+
+double_pr = cellfun(@double, {sigma_1, sigma_2, sigma_3}, "UniformOutput", 0)
+[sigma_1_double, sigma_2_double, sigma_3_double] = double_pr{:}
+
+R_1 = 1/2*(sigma_2_double - sigma_3_double)
+R_2 = 1/2*(sigma_1_double - sigma_3_double)
+R_3 = 1/2*(sigma_1_double - sigma_2_double)
+
+O_1 = [1/2*(sigma_2_double + sigma_3_double), 0]
+O_2 = [1/2*(sigma_1_double + sigma_3_double), 0]
+O_3 = [1/2*(sigma_1_double + sigma_2_double), 0]
+
+angle = (0:pi/1800:pi);
+
+x_1 = O_1(1) * ones([1 1801]) + R_1 * cos(angle);
+y_1 = R_1 * sin(angle);
+
+x_2 = O_2(1) * ones([1 1801]) + R_2 * cos(angle);
+y_2 = R_2 * sin(angle);
+
+x_3 = O_3(1) * ones([1 1801]) + R_3 * cos(angle);
+y_3 = R_3* sin(angle);
+
+plot(x_1, y_1, '-k', "LineWidth", 1.5)
+plot(x_2, y_2, '-k', "LineWidth", 1.5)
+plot(x_3, y_3, '-k', "LineWidth", 1.5)
+fill([x_1, x_2, x_3], [y_1, y_2, y_3], 'k', 'FaceAlpha', 0.5);
+
+% exportgraphics(gcf, "images/The_Mohrs_Diagram.pdf", "ContentType","vector")
+% exportgraphics(gcf, "images/The_Mohrs_Diagram.eps", "ContentType","vector")
+% exportgraphics(gcf, "images/The_Mohrs_Diagram.png", "Resolution", 1200)
